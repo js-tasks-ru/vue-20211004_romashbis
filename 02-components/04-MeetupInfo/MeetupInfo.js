05c4cc0ed4ba8e018ebf74dcbb0f9b15f9d6f358
Elijah Romashchenko <romashbis@gmail.com>
2021-12-05 14:32:52 +0200

    feat: add exrs.#4 & refactor the exrs.#3

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,42 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      default: null,
+    },
+    place: {
+      type: String,
+      default: null,
+    },
+    date: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  computed: {
+    convertedDate() {
+      let newDate = new Date(this.date);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return newDate.toLocaleDateString(navigator.language, options); // <-- у меня локаль Английская, поэтому захардкодил на русскую
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="convertedDate"> {{ convertedDate }} </time>
       </li>
     </ul>`,
 });
